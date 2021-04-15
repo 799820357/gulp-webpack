3536d21557f555b94263d4211c31473cc7b3b6ef
lichen105519 <lichen105519@pwrd.com>
2021-04-15 15:21:28 +0800

    更新

--- webpack-config/optimization.js
+++ webpack-config/optimization.js
@@ -1,23 +1,24 @@
 const TerserPlugin = require("terser-webpack-plugin");
 module.exports = projectInfo => {
     return {
-        // splitChunks: {
-        //     chunks: "all",
-        //     cacheGroups: { // 缓存组
-        //         vendors: {
-        //             test: /[\\/]node_modules[\\/]/,
-        //             priority: -10,
-        //             name(module, chunks, cacheGroupKey) {
-        //                 return cacheGroupKey;
-        //             }
-        //         },
-        //         default: {
-        //             minChunks: 2,
-        //             priority: -20,
-        //             reuseExistingChunk: true
-        //         }
-        //     }
-        // },
+        splitChunks: {
+            chunks: "all",
+            minSize:100,
+            cacheGroups: { // 缓存组
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    name(module, chunks, cacheGroupKey) {
+                        return cacheGroupKey;
+                    }
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        },
         minimizer: [
             new TerserPlugin({
                 parallel: true, //开启并行压缩
